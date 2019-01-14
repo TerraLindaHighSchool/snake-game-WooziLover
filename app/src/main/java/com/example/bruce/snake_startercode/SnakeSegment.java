f8738a81d59c65c4cee44d39c12e030fc977ada3
WooziLover <oohty15@gmail.com>
2019-01-13 18:03:24 -0800

    Lab 1 is complete and ready for grading.

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
@@ -1,4 +1,54 @@
 package com.example.bruce.snake_startercode;
 
 public class SnakeSegment {
+
+    private BodyParts mBodyParts;
+    int mDegrees, mXLoc, mYLoc;
+
+    public SnakeSegment (BodyParts bodyParts, int degrees, int xLoc, int yLoc){
+        mBodyParts = bodyParts;
+        mDegrees = degrees;
+        mXLoc = xLoc;
+        mYLoc = yLoc;
+    }
+
+    /*************************************
+     * Enum
+     ************************************/
+
+    public enum BodyParts {
+        HEAD, BODY, TAIL
+    }
+
+    /************************************
+     * Getters and Setters
+     ***********************************/
+
+    public BodyParts getBodyParts(){
+        return mBodyParts;
+    }
+
+    public void setDegrees(int d){
+        mDegrees = d;
+    }
+
+    public int getDegrees(){
+        return mDegrees;
+    }
+
+    public void setXLoc(int x){
+        mXLoc = x;
+    }
+
+    public int getXLoc(){
+        return mXLoc;
+    }
+
+    public void setYLoc(int y){
+        mYLoc = y;
+    }
+
+    public int getYLoc(){
+        return mYLoc;
+    }
 }
---
+++ app/src/test/java/com/example/bruce/snake_startercode/SnakeSegmentTest.java
@@ -0,0 +1,79 @@
+package com.example.bruce.snake_startercode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SnakeSegmentTest {
+
+    SnakeSegment[] snakeSegments = {
+        new SnakeSegment (SnakeSegment.BodyParts.HEAD, 0,1,1),
+        new SnakeSegment (SnakeSegment.BodyParts.BODY,90,2,2),
+        new SnakeSegment (SnakeSegment.BodyParts.TAIL,180,3,3),
+        new SnakeSegment (SnakeSegment.BodyParts.HEAD,270,4,4)};
+
+
+    @Test
+    public void getBody() {
+        SnakeSegment.BodyParts[] actualValue = {SnakeSegment.BodyParts.values()[0],SnakeSegment.BodyParts.values()[1], SnakeSegment.BodyParts.values()[2]};
+        SnakeSegment.BodyParts[] expectedValue = new SnakeSegment.BodyParts[3];
+        for(int i = 0; i < snakeSegments.length - 1;i++){
+            expectedValue[i] = snakeSegments[i].getBodyParts();
+        }
+        assertArrayEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    public void setDegrees() {
+       snakeSegments[0].setDegrees(270);
+       assertTrue(snakeSegments[0].getDegrees() == 270);
+       snakeSegments[1].setDegrees(180);
+       assertTrue(snakeSegments[1].getDegrees() == 180);
+       snakeSegments[2].setDegrees(90);
+       assertTrue(snakeSegments[2].getDegrees() == 90);
+       snakeSegments[3].setDegrees(0);
+       assertTrue(snakeSegments[3].getDegrees() == 0);
+    }
+
+    @Test
+    public void getDegrees() {
+        int[] actualValue = {0, 90, 180, 270};
+        int[] expectedValue = new int[4];
+        for(int i = 0; i < snakeSegments.length;i++){
+            expectedValue[i] = snakeSegments[i].getDegrees();
+        }
+        assertArrayEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void setXLoc() {
+        snakeSegments[3].setXLoc(2);
+        assertTrue(snakeSegments[3].getXLoc() == 2);
+    }
+
+    @Test
+    public void getXLoc() {
+        int[] actualValue = {1, 2, 3, 4};
+        int[] expectedValue = new int[4];
+        for(int i = 0; i < snakeSegments.length;i++){
+            expectedValue[i] = snakeSegments[i].getXLoc();
+        }
+        assertArrayEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void setYLoc() {
+        snakeSegments[1].setYLoc(1);
+        assertTrue(snakeSegments[1].getYLoc() == 1);
+    }
+
+    @Test
+    public void getYLoc() {
+        int[] actualValue = {1, 2, 3, 4};
+        int[] expectedValue = new int[4];
+        for(int i = 0; i < snakeSegments.length;i++){
+            expectedValue[i] = snakeSegments[i].getYLoc();
+        }
+        assertArrayEquals(expectedValue, actualValue);
+    }
+}
