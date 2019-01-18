a2ac86cf3d22136b702598df0c0cf4fcff8cf622
tl00456 <oohty15@gmail.com>
2019-01-18 14:22:39 -0800

    Lab 2 Rough Draft

--- app/src/androidTest/java/com/example/bruce/snake_startercode/SnakeSegmentTest.java
+++ app/src/androidTest/java/com/example/bruce/snake_startercode/SnakeSegmentTest.java
@@ -7,26 +7,32 @@ import static org.junit.Assert.*;
 public class SnakeSegmentTest {
 
     SnakeSegment[] snakeSegments = {
-        new SnakeSegment(SnakeSegment.BodyParts.HEAD, 0,1,1),
-        new SnakeSegment(SnakeSegment.BodyParts.BODY, 90,1,1),
-        new SnakeSegment(SnakeSegment.BodyParts.TAIL,180,1,1),
-        new SnakeSegment(SnakeSegment.BodyParts.HEAD, 270,1,1)};
+            new SnakeSegment (SnakeSegment.BodyParts.HEAD, 0,1,1),
+            new SnakeSegment (SnakeSegment.BodyParts.BODY,90,2,2),
+            new SnakeSegment (SnakeSegment.BodyParts.TAIL,180,3,3),
+            new SnakeSegment (SnakeSegment.BodyParts.HEAD,270,4,4)};
 
 
     @Test
-    public void getBody() {
-    SnakeSegment.BodyParts[] actualValue = {SnakeSegment.BodyParts.values()[0],SnakeSegment.BodyParts.values()[1], SnakeSegment.BodyParts.values()[2]};
-    SnakeSegment.BodyParts[] expectedValue = new SnakeSegment.BodyParts[3];
-    for(int i = 0; i < snakeSegments.length; i++) {
-        expectedValue[i] = snakeSegments[i].getBody();
-    }
-    assertArrayEquals(expectedValue, actualValue);
+    public void getBodyParts() {
+        SnakeSegment.BodyParts[] actualValue = {SnakeSegment.BodyParts.values()[0],SnakeSegment.BodyParts.values()[1], SnakeSegment.BodyParts.values()[2]};
+        SnakeSegment.BodyParts[] expectedValue = new SnakeSegment.BodyParts[3];
+        for(int i = 0; i < snakeSegments.length - 1;i++){
+            expectedValue[i] = snakeSegments[i].getBodyParts();
+        }
+        assertArrayEquals(expectedValue,actualValue);
     }
 
     @Test
     public void setDegrees() {
-    /*int expected = snakeSegments.setDegrees(90);
-    assertTrue(expected, );*/
+        snakeSegments[0].setDegrees(270);
+        assertTrue(snakeSegments[0].getDegrees() == 270);
+        snakeSegments[1].setDegrees(180);
+        assertTrue(snakeSegments[1].getDegrees() == 180);
+        snakeSegments[2].setDegrees(90);
+        assertTrue(snakeSegments[2].getDegrees() == 90);
+        snakeSegments[3].setDegrees(0);
+        assertTrue(snakeSegments[3].getDegrees() == 0);
     }
 
     @Test
@@ -41,20 +47,33 @@ public class SnakeSegmentTest {
 
     @Test
     public void setXLoc() {
-
+        snakeSegments[3].setXLoc(2);
+        assertTrue(snakeSegments[3].getXLoc() == 2);
     }
 
     @Test
     public void getXLoc() {
-        int[] actualValue = {}
+        int[] actualValue = {1, 2, 3, 4};
+        int[] expectedValue = new int[4];
+        for(int i = 0; i < snakeSegments.length;i++){
+            expectedValue[i] = snakeSegments[i].getXLoc();
+        }
+        assertArrayEquals(expectedValue, actualValue);
     }
-
 
     @Test
     public void setYLoc() {
+        snakeSegments[1].setYLoc(1);
+        assertTrue(snakeSegments[1].getYLoc() == 1);
     }
 
     @Test
     public void getYLoc() {
+        int[] actualValue = {1, 2, 3, 4};
+        int[] expectedValue = new int[4];
+        for(int i = 0; i < snakeSegments.length;i++){
+            expectedValue[i] = snakeSegments[i].getYLoc();
+        }
+        assertArrayEquals(expectedValue, actualValue);
     }
 }
--- app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
@@ -1,7 +1,56 @@
 package com.example.bruce.snake_startercode;
 
+import android.content.SharedPreferences;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class SnakeGame {
-  
+
+  private int mMillisDelay, mSpriteDim, mScore, mLevel, mCountdown;
+  protected int[] mAppleCoord;
+  protected List<SnakeSegment> mSnake;
+  protected SnakeSegment mBodyPart;
+
+  protected int getMillisDelay(){
+    return mMillisDelay;
+  }
+
+  protected int getSpriteDim(){
+    return mSpriteDim;
+  }
+
+  protected int[] getAppleCoord(){
+    return mAppleCoord;
+  }
+
+  protected boolean getGameOver(){
+    return false;
+  }
+
+  protected int getScore(){
+    return mScore;
+  }
+
+  protected int getLevel(){
+    return mLevel;
+  }
+
+  protected int getCountdown(){
+    return mCountdown;
+  }
+
+    public SnakeSegment getBodyPart() {
+        return mBodyPart;
+    }
+
+  protected List<SnakeSegment> getSnake(){
+      return mSnake;
+  }
+
   public SnakeGame(int beginningDirection, int beginningSpriteDim, int beginningX, int beginningY, int width, int height){
   
   }
