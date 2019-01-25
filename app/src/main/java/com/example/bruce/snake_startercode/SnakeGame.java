ec43b684aa8939a93f98f0c093e1b39600c8919e
tl00456 <oohty15@gmail.com>
2019-01-25 12:37:05 -0800

    Lab 3 has been pushed and is ready for grading.

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
@@ -12,7 +12,7 @@ import static android.content.Context.POWER_SERVICE;
 
 public class SnakeGame {
 
-  private int mMillisDelay, mSpriteDim, mScore, mLevel, mCountdown, mBOARD_WIDTH, mBOARD_HEIGHT, mXValue,mYValue,mDegrees, mXMax, mYMax, mRandomXCoordinate, mRandomYCoordinate;;
+  private int mMillisDelay, mSpriteDim, mScore, mLevel, mCountdown, mBOARD_WIDTH, mBOARD_HEIGHT, mXMax, mYMax, mRandomXCoordinate, mRandomYCoordinate;;
   protected int[] mAppleCoord;
   protected ArrayList<SnakeSegment> mSnake = new ArrayList();
   private SnakeSegment mBodyParts;
@@ -21,20 +21,20 @@ public class SnakeGame {
 
   public SnakeGame(int beginningDirection, int beginningSpriteDim, int beginningX, int beginningY, int width, int height){
     mSpriteDim = beginningSpriteDim;
+
     mBOARD_WIDTH = width;
     mBOARD_HEIGHT = height;
     mScore = 0;
     mLevel = 1;
     mCountdown = 12;
     mMillisDelay = 750;//400
-    mXMax = 10;
-    mYMax = 10;
+    mXMax = width / beginningSpriteDim;
+    mYMax = height / beginningSpriteDim;
     mAppleCoord = new int[2];
-    mSnake.add(new SnakeSegment(SnakeSegment.BodyParts.TAIL, beginningDirection, beginningX - 2, beginningY));
-    mSnake.add(new SnakeSegment(SnakeSegment.BodyParts.BODY, beginningDirection, beginningX - 1, beginningY));
     mSnake.add(new SnakeSegment(SnakeSegment.BodyParts.HEAD, beginningDirection, beginningX, beginningY));
+    mSnake.add(new SnakeSegment(SnakeSegment.BodyParts.BODY, beginningDirection, beginningX - 1, beginningY));
+    mSnake.add(new SnakeSegment(SnakeSegment.BodyParts.TAIL, beginningDirection, beginningX - 2, beginningY));
     setAppleCoord();
-    play();
 
   }
 
@@ -79,29 +79,33 @@ public class SnakeGame {
   }
     
   protected boolean play() {
-    SnakeSegment mType;
     //
       for (int i = 0; i < mSnake.size(); i++) {
-        switch(mDegrees){
+        SnakeSegment.BodyParts bp = mSnake.get(i).getBodyParts();
+        int degrees = mSnake.get(i).getDegrees();
+        int x = mSnake.get(i).getXLoc();
+        int y = mSnake.get(i).getYLoc();
+
+
+        switch(degrees){
           case 0:
             //This is the start of experimenting.
             //This is the end
-            mSnake.get(i).setXLoc(mXValue++);
+            mSnake.get(i).setXLoc(++x);
             break;
 
-          case 1:
-            mSnake.get(i).setYLoc(mYValue++);
+          case 90:
+            mSnake.get(i).setYLoc(++y);
             break;
 
-          case 2:
-            mSnake.get(i).setXLoc(mXValue++);
-            mSnake.get(i).setYLoc(mYValue++);
+          case 180:
+            mSnake.get(i).setXLoc(--x);
 
-          case 3:
-            mSnake.get(i).setYLoc(mYValue++);
+          case 270:
+            mSnake.get(i).setYLoc(--y);
         }
       }
-    if(mSnake.get(1).getXLoc() >= mXMax){
+    if(mSnake.get(0).getXLoc() >= mXMax){
       return true;
     }
       return false;
@@ -109,11 +113,8 @@ public class SnakeGame {
 
     //This has something to do with the GameActivity.java the //Draw Apple section.
     private void setAppleCoord(){
-      mRandomXCoordinate = (int) (11 * Math.random() + 1);
-      mRandomYCoordinate = (int) (11 * Math.random() + 1);
-      if(mRandomXCoordinate >= mXMax || mRandomYCoordinate >= mYMax){
-        mRandomXCoordinate -= 1;
-        mRandomYCoordinate -= 1;
-      }
+      mAppleCoord[0] = (int) ((mXMax - 1) * Math.random() + 1) * mSpriteDim;
+      mAppleCoord[1] = (int) ((mYMax - 1) * Math.random() + 1) * mSpriteDim;
+
     }
 }
